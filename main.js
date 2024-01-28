c587bebe9ca8c2635ea1360db7f1712a8e24816a
Tim <timothy.ndarau@student.moringaschool.com>
2024-01-29 01:12:38 +0300

    all files changed

--- main.js
+++ main.js
@@ -1,21 +1,52 @@
-function generatepassword(length, lower, upper, number, symbol) {
-    let generatepassword = "";
+function generatePassword(length, lower, upper, number, symbol) {
+    let generatedPassword = "";
     const typesCount = lower + upper + number + symbol;
-    const typesArr= [{lower}, {upper}, {number}, {symbol}].filter(
+    
+    // Define the randomFunc object with character generation functions
+    const randomFunc = {
+        lower: getRandomLower,
+        upper: getRandomUpper,
+        number: getRandomNumber,
+        symbol: getRandomSymbol,
+    };
+
+    const typesArray = [{ lower }, { upper }, { number }, { symbol }].filter(
         (item) => Object.values(item)[0]
     );
+
     if (typesCount === 0) {
         return "";
     }
-    for (let i = 0 ; i < length; i++ ){ 
-        typesArr.forEach((type) => {
+
+    for (let i = 0; i < length; i++) {
+        typesArray.forEach((type) => {
             const funcName = Object.keys(type)[0];
-            generatepassword =+ randomFunction[funcName]();
+            generatedPassword += randomFunc[funcName]();
         });
     }
-    return generatepassword
-      .slice(0, length)
-      .split("")
-      .sort(() => Map.random() - 0.5)
-      .join("");
+
+    return generatedPassword
+        .slice(0, length)
+        .split("")
+        .sort(() => Math.random() - 0.5)
+        .join("");
 }
+
+// Character generation functions
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+    const symbols = "!@#$%^&*()_+[]{}|;:,.<>?/";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+

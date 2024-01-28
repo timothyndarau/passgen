dd0f8df3fc4982240e981813ad00a627d15d001b
Tim <timothy.ndarau@student.moringaschool.com>
2024-01-29 01:14:40 +0300

    js file updated

--- main.js
+++ main.js
@@ -1,52 +1,33 @@
-function generatePassword(length, lower, upper, number, symbol) {
-    let generatedPassword = "";
-    const typesCount = lower + upper + number + symbol;
-    
-    // Define the randomFunc object with character generation functions
-    const randomFunc = {
-        lower: getRandomLower,
-        upper: getRandomUpper,
-        number: getRandomNumber,
-        symbol: getRandomSymbol,
-    };
+console.clear();
+const sliderProps = {
+  fill: "#0B1EDF",
+  background: "rgba(255, 255, 255, 0.214)",
+};
 
-    const typesArray = [{ lower }, { upper }, { number }, { symbol }].filter(
-        (item) => Object.values(item)[0]
-    );
+const slider = document.querySelector(".range__slider");
 
-    if (typesCount === 0) {
-        return "";
-    }
+const sliderValue = document.querySelector(".length__title");
 
-    for (let i = 0; i < length; i++) {
-        typesArray.forEach((type) => {
-            const funcName = Object.keys(type)[0];
-            generatedPassword += randomFunc[funcName]();
-        });
-    }
+slider.querySelector("input").addEventListener("input", (event) => {
+  sliderValue.setAttribute("data-length", event.target.value);
+  applyFill(event.target);
+});
 
-    return generatedPassword
-        .slice(0, length)
-        .split("")
-        .sort(() => Math.random() - 0.5)
-        .join("");
+applyFill(slider.querySelector("input"));
+
+function applyFill(slider) {
+  const percentage =
+    (100 * (slider.value - slider.min)) / (slider.max - slider.min);
+  const bg = `linear-gradient(90deg, ${sliderProps.fill} ${percentage}%, ${
+    sliderProps.background
+  } ${percentage + 0.1}%)`;
+  slider.style.background = bg;
+  sliderValue.setAttribute("data-length", slider.value);
 }
 
-// Character generation functions
-function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-    const symbols = "!@#$%^&*()_+[]{}|;:,.<>?/";
-    return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
